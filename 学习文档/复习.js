acf7cb825ff4ad76094ace9c99b3c1a96894f6d4
qinyu <947794823@qq.com>
2022-01-13 17:30:48 +0800

    'updata'

--- 学习文档/复习.js
+++ 学习文档/复习.js
@@ -1,3 +1,4 @@
+const { repeat } = require("core-js/core/string")
 
 //#region html
     header
@@ -48,6 +49,17 @@
         text-indent //首航缩进 :2em
         text-decoration //文本描装饰 underline(下划线)/overline(顶划线)/line-through(中划线)/none
         font-style  //设置倾斜  italic(倾斜)/normal
+        line-height //行高  可以是10px,也可以是1(倍数)
+        font    //是否倾斜 是否加粗 字号(必填项)/行高 字体(必填项) 单向和复合项同时存在是,要先写复合项
+    边框属性
+        border  //粗细 样式 颜色 5px solid red
+        border-left/top/right/bottom    //边框没有none
+    背景
+        background-color    
+        background-image  //url()   repeat平铺(默认)/repeat-x/repeat-y/no-repeat
+        background-repeat    //no-repeat
+        background-postion  //100px 0px
+        background  //背景色 背景图 平铺方式 水平位置 垂直位置 yellow url() no-repeat 10px 10px
     设置颜色
         color   //blue/rgb(255,255,255)/#FF0000
     盒子基本属性
@@ -59,6 +71,9 @@
             span/a/b/strong/i/em/u/ins/s/del(加粗/倾斜/下划线/删除)
         行内块元素
             img/表单标签
+    float   //浮动
+        //影响:1父元素美誉宽高,子元素浮动,父元素坍塌
+        
     选择器
         标签选择器  //标签名{}  span{}
         类选择器    //.类名{}
@@ -70,6 +85,34 @@
 
         权重    //!important > 行内样式 > id选择器 > 类选择器 > 标签选择器
         块元素的默认面积组成    //margin + border + padding + content
+    状态伪类
+        未访问状态 //a:link{}
+        访问后状态 //a:visited{}
+        鼠标移入状态 //a:hover{}
+        鼠标按下(触发状态) //a:active{}
+    
 //#endregion
 
-35
+//#region 
+    盒子模型    //盒子在网页中的尺寸 = content(设置的宽高) + padding区域 + border区域
+        padding //内边距   上/右/下/左(4个属性) 上/左右/下(3个属性) 上下/左右(2个属性) 上右下左(1个值)
+            padding-left/top/right/botom
+            padding减宽度   //块元素没有固定宽度:不会撑宽盒子,会从content减去padding值,宽度不变
+                            //块元素有固定宽度:盒子尺寸会变大
+        margin  //外边距    上/右/下/左(4个属性) 上/左右/下(3个属性) 上下/左右(2个属性) 上右下左(1个值)
+            margin-left/top/right/botom
+            外边距合并  //上面盒子设置的下边距 和 下面盒子设置的上边距会合并
+            外边距坍塌  //嵌套的连个块元素,子元素设置向上的外边距,父元素会跟着掉下来
+                //解决方案:1给父元素设置上边框 2.给父元素设置overflow属性 3.给父元素设置浮动 4.给子元素设置浮动
+            块元素水平居中
+                //水平居中 margin:0 auto
+        overflow //溢出属性    hidden(溢出隐藏)/scroll(溢出滚动)/auto(溢出滚动)
+            overflow-x/overflow-y
+        底部留白 //img
+            //解决方案:1件img转为块元素,display:block 2将box字体设置为0
+    
+
+
+
+
+//#endregion
